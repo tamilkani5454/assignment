45109a14f87bed5f4b26c2f78bbfd9e879dfbbbd
tamilkani5454 <donkani893@gmail.com>
2026-03-21 12:40:45 +0530

    last commit1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  "routes": [
+    { "src": "/assets/(.*)", "dest": "/assets/$1" },
+    { "src": "/(.*)", "dest": "/" }
+  ]
 })
